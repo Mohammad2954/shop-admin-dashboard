0c32122ef7bcc8444ba2854cdbcb226004827c61
mohammadjavad <mohamad2134javad@gmail.com>
2026-09-26 16:17:57 +0330

    feat: added menu for sidebar

--- src/feachers/Sidebar/components/MenuSidebar.jsx
+++ src/feachers/Sidebar/components/MenuSidebar.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { FaHome } from "react-icons/fa";
+import { FaBasketShopping } from "react-icons/fa6";
+import { HiUsers } from "react-icons/hi";
+import { IoTicketSharp } from "react-icons/io5";
+import { VscCommentDiscussion } from "react-icons/vsc";
 
 function MenuSidebar() {
-  return <div>MenuSidebar</div>;
+  return (
+    <div>
+      <ul>
+        <p className="my-2 text-sm font-bold">منو اصلی :</p>
+        <li className="flex items-center gap-2 cursor-pointer hover:bg-gray-200 transition duration-500 rounded-lg px-3 py-2  group">
+          {" "}
+          <FaHome className=" group-hover:text-green-600 transition duration-500" />
+          داشبورد{" "}
+        </li>
+        <li className="flex items-center gap-2 cursor-pointer hover:bg-gray-200 transition duration-500 rounded-lg px-3 py-2  group">
+          {" "}
+          <FaBasketShopping className=" group-hover:text-green-600 transition duration-500" />
+          محصولات
+        </li>
+        <li className="flex items-center gap-2 cursor-pointer hover:bg-gray-200 transition duration-500 rounded-lg px-3 py-2  group">
+          <HiUsers className=" group-hover:text-green-600 transition duration-500" />
+          کاربران
+        </li>
+        <li className="flex items-center gap-2 cursor-pointer hover:bg-gray-200 transition duration-500 rounded-lg px-3 py-2  group">
+          <IoTicketSharp className=" group-hover:text-green-600 transition duration-500" />
+          تیکت ها
+        </li>
+        <li className="flex items-center gap-2 cursor-pointer hover:bg-gray-200 transition duration-500 rounded-lg px-3 py-2  group">
+          <VscCommentDiscussion className=" group-hover:text-green-600 transition duration-500" />
+          کامنت ها
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default MenuSidebar;
--- src/feachers/Sidebar/components/SidebarHeader.jsx
+++ src/feachers/Sidebar/components/SidebarHeader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router";
 function SidebarHeader() {
   return (
-    <Link className="flex items-center gap-1 border-b pb-2">
+    <Link to={"/"} className="flex items-center gap-1 border-b pb-2">
       <img src="./image/icon/header-icon.webp" alt="" />
       <h3 className="font-bold ">داشبورد ادمین</h3>
     </Link>
